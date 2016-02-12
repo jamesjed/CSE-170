fb701e26ee55608d5153f7d791499b67a78217d6
James Edward Natanauan <jamesjedn@gmail.com>
2016-02-12 02:52:24 -0800

    fdsfs

--- public/js/whiteboard.js
+++ public/js/whiteboard.js
@@ -1,39 +1,94 @@
 var canvas = document.getElementById("whiteboard");
 
 var context = canvas.getContext("2d");
+var context2 = canvas.getContext("2d");
 
 var radius = 3;
 var dragging = false;
+var endEmit = false;
 
 context.canvas.width  = (window.innerWidth)*.9;
 context.canvas.height = (window.innerHeight)*.7;
 
 context.lineWidth = radius*2;
 
-var putPoint = function(e){
+var socket = io.connect();
+var $whiteboard = $('#whiteboard');
 
+var prevPointMouse = {};
+
+var prevPointTouch = {};
+
+prevPointMouse.x = 1;
+
+// Receive data from other user drawings and use the data
+// to draw data on your own screen
+socket.on('mouseReceive', function(data){
+	//console.log(data.x + " " + data.y);
+
+	/*
+	context.beginPath();
+	context.lineTo(data.x, data.y);
+	context.stroke();
+	context.beginPath(); */
+
+	context.arc(data.x, data.y, data.radius, data.start, data.end);
+	context.fill();
+	context.beginPath();
+
+});
+
+var putPoint = function(e){
 	if(dragging){
 		context.lineTo(e.clientX, e.clientY);
 		context.stroke();
+
+		var data = {
+			x: e.clientX,
+			y: e.clientY,
+			radius: radius,
+			start: 0,
+			end: 2 * Math.PI
+		}
+		socket.emit('mouseDraw', data);
 		context.beginPath();
-	// Offset x and y are the coordinates of the mouse relative
-	// to the browser window
+
+		// Offset x and y are the coordinates of the mouse relative
+		// to the browser window
 		context.arc(e.clientX, e.clientY, radius, 0, 2 * Math.PI);
 		context.fill();
 		context.beginPath();
 		context.moveTo(e.clientX, e.clientY);
 	}
+
 }
 
 var startDraw = function(e){
 	dragging = true;
+	endEmit = true;
 	putPoint(e);
 }
 
 var stopDraw = function(){
 	dragging = false;
+	endEmit = false;
 	context.beginPath();
 }
+
+socket.on('touchReceive', function(data){
+	//console.log(data.x + " " + data.y);
+	//context.beginPath();
+
+	/*
+	context.lineTo(data.x, data.y);
+	context.stroke();
+	context.beginPath(); */
+
+	context.arc(data.x, data.y, data.radius, data.start, data.end);
+	context.fill();
+	context.beginPath();
+
+});
 
 var touchPutPoint = function(e){
 	e.preventDefault();
@@ -41,11 +96,20 @@ var touchPutPoint = function(e){
 	if(dragging){
 		context.lineTo(touch.pageX, touch.pageY);
 		context.stroke();
+
+		var data = {
+			x: touch.pageX,
+			y: touch.pageY,
+			radius: radius,
+			start: 0,
+			end: 2 * Math.PI
+		}
 		context.beginPath();
 
-	// Offset x and y are the coordinates of the mouse relative
-	// to the browser window
+		// Offset x and y are the coordinates of the mouse relative
+		// to the browser window
 		context.arc(touch.pageX, touch.pageY, radius, 0, 2 * Math.PI);
+		socket.emit('touchDraw', data);
 		context.fill();
 		context.beginPath();
 		context.moveTo(touch.pageX, touch.pageY);
@@ -60,7 +124,7 @@ var touchStartDraw = function(e){
 	touchPutPoint(e);
 }
 
-var touchStopDraw = function(){
+var touchStopDraw = function(e){
 	e.preventDefault();
 	context.beginPath();
 	dragging = false;
@@ -74,3 +138,4 @@ canvas.addEventListener('mouseup', stopDraw);
 canvas.addEventListener('touchmove', touchPutPoint);
 canvas.addEventListener('touchstart', touchStartDraw);
 canvas.addEventListener('touchend', touchStopDraw);
+
