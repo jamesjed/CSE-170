536f6f6e3f3b523b2d709ad97e6a0b3e4489b588
James Edward Natanauan <jamesjedn@gmail.com>
2016-03-02 22:38:55 -0800

    new account for ga

--- routes/analytics.js
+++ routes/analytics.js
@@ -1,13 +1,27 @@
 exports.viewA = function(req, res){
-    console.log("In A");
-    var versionA = true;
-    res.render("discussion", {versionA : true, layout: false})
+	var randomNum = Math.random();
+
+	if (randomNum > 0.5){
+ 	   res.render("discussion", {layout: false})
+ 	   console.log(randomNum);
+	}
+	else {
+		res.redirect('/chat');
+	}
  };
 
  exports.viewB = function(req, res){
-    console.log("In B");
-    var versionB = true;
-    res.render("chat_sample", {versionB : true, layout: false});
+
+ 	var randomNum = Math.random();
+
+ 	if (randomNum > 0.5){
+    	res.render("chat_sample", {versionB : true, layout: false});
+    	   console.log(randomNum);
+    }
+    else 
+    {
+    	res.redirect('/discuss');
+    }
  };
 
 
